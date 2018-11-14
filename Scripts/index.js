9aa9afe5b3465ee401effba453f7f9d9f8d262a9
Ivan Miatselski <Ivan_Miatselski2@epam.com>
2018-11-15 00:23:36 +0300

    Deleted additional html page

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,7 +11,7 @@ function getAllSources(){
 
             let soursesMarkup = sources.reduce((markup, current) => {
                 return markup.concat(`<div id="${current.id}" class="news-container"><img class="preview" src="https://besticon-demo.herokuapp.com/icon?url=${current.url}&amp;size=70..120..200">
-                    <div class="title"><a href="#" onclick='location.href = "./article.html?articleId=${current.id}&apiKey=${apiKey}"'><strong>"${current.name}"</strong></a></div></div>`);
+                    <div class="title"><a href="#" onclick='getContentSource("${current.id}");'><strong>"${current.name}"</strong></a></div></div>`);
             }, '');
     
             document.getElementById("source-container").innerHTML = soursesMarkup;
@@ -19,6 +19,36 @@ function getAllSources(){
         .catch(error => {
             console.log(error);
         });
+}
+
+function getContentSource(articleId){
+    let urlRequest = `https://newsapi.org/v1/articles?source=${articleId}&apiKey=${apiKey}`;
+    fetch(urlRequest, { method: "GET" })
+        .then((response) => response.json())
+        .then((data) => {
+            let articles = data.articles.map(article => {
+                return {image: article.urlToImage, title: article.title, description: article.description, url: article.url};
+            });
+    
+            let articlesHTML = getContentSourceHTML(articles);
+            document.getElementById("source-container").innerHTML = articlesHTML;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+function getContentSourceHTML(articles){                          
+    if (articles != null){
+        return articles.reduce((markup, current) => {
+            return markup.concat(`<div class="article"><div class="article-image-div"><img src="${current.image}" class="article-image"></div><div class="article-content"><div class="article-title">
+                <h2 class="title-style">${current.title}</h2></div><div class="article-description description-style">${current.description}</div>
+                <div class="article-reference"><a href="${current.url}" class="reference-style">Redirect to article >></a></div></div></div>`);
+        }, '');
+    }
+    else {
+        return "<div>Articles didn't found</div>";
+    }
 }
 
 window.onload = getAllSources;
